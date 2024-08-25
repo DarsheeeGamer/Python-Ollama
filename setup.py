dc7aa92cfae623ce5255f6f3b9cac349f08e7bba
Kaede Dev Kento Hinode <139364723+DarsheeeGamer@users.noreply.github.com>
2024-08-25 10:18:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Python-Ollama",
-    version="0.1.0",
+    version="0.2.0",
     description="Python client for the Ollama API",
     author="Kaede Dev Kento Hinode",
     author_email="cleaverdeath@gmail.com",
